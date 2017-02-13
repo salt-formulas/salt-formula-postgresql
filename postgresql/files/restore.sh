4a2f061a458349308ddd02ae1958237bb18eb996
Adam Tengler <adam.tengler@tcpcloud.eu>
2017-02-13 18:05:08 +0000

    Improved backuping

--- postgresql/files/restore.sh
+++ postgresql/files/restore.sh
@@ -33,11 +33,20 @@ touch /root/postgresql/flags/{{ database.name }}-installed
 {%- set dest_name = db_name + ".pg_dump.gz" %}
 {%- set target = "/root/postgresql/data/" %}
 
+{%- if database.initial_data.auth is defined %}
+export AWS_ACCESS_KEY_ID={{ database.initial_data.auth.awsaccesskeyid }}
+export AWS_SECRET_ACCESS_KEY={{ database.initial_data.auth.awssecretaccesskey }}
+{%- endif %}
+
+{%- if database.initial_data.engine == "backupninja" %}
 scp backupninja@{{ database.initial_data.source }}:/srv/backupninja/{{ host }}/var/backups/postgresql/postgresql.{{ age }}/{{ source_name }} {{ target }}{{ dest_name }} 
-
 gunzip -d -1 -f {{ target }}{{ dest_name }}
-
 pg_restore /root/postgresql/data/{{ db_name }}.pg_dump --dbname={{ db_name }} --host=localhost --username={{ database.users[0].name }} --no-password -c
+{%- else %}
+duplicity --s3-unencrypted-connection --s3-use-new-style --s3-european-buckets s3+http://{{ database.initial_data.source }}/{{ host }} {{ target }}{{ dest_name }} 
+gunzip -d -1 -f {{ target }}{{ dest_name }}/var/backups/postgresql/{{ source_name }}
+pg_restore /root/postgresql/data/{{ dest_name }}/var/backups/postgresql/{{ db_name }}.pg_dump --dbname={{ db_name }} --host=localhost --username={{ database.users[0].name }} --no-password -c
+{%- endif %}
 
 touch /root/postgresql/flags/{{ db_name }}-installed
 
